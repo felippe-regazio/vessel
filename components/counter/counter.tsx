04eb8a6f27f35d3898057a96912ffda8335fec6a
Felippe Regazio <felippe.moraes@zoho.com>
2023-01-27 00:12:21 -0300

    sync

--- components/counter/counter.tsx
+++ components/counter/counter.tsx
@@ -2,9 +2,8 @@ import { WC } from 'wcwc';
 import style from './style.scss';
 
 class Counter extends WC {
-  static styles = [ style ];
-  static attrs = [ 'start' ];
-
+  static $styles = [ style ];
+  
   data: { count: number } = this.$({
     count: this.props.start || 0
   });
@@ -28,4 +27,17 @@ class Counter extends WC {
   }
 }
 
-Counter.expose('my-custom-element');
+Counter.expose('my-custom-element', {
+  shadow: { mode: 'open' },
+  
+  props: {
+    start: {
+      default: 40
+    },
+
+    bg: {
+      default: '#eee', 
+      css: true
+    }
+  }
+});
