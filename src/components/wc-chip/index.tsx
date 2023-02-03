3117cea1be109dbb83fb6ff0c0f449e53aa6a7dc
Felippe Regazio <felippe.moraes@zoho.com>
2023-02-02 23:32:22 -0300

    testing

--- src/components/wc-chip/index.tsx
+++ src/components/wc-chip/index.tsx
@@ -1,15 +1,10 @@
 import style from './style.scss';
-// import CloseIcon from '../../partials/icons/CloseIcon';
 
 class WCChip extends WC {
   static styles = [ style ];
   
   render() {
-    return (
-      <>
-        a
-      </>
-    )
+    return this.props.children;
   }
 }
 
